b67442999fcb6cbb220b0be46d94b5591512adfc
Chris Talkington <chris@talkingtontech.com>
2020-03-20 13:41:01 -0500

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,7 @@ import directv.utils as utils
 def test_combine_channel_number() -> None:
     """Test the merging of channel numbers."""
     assert utils.combine_channel_number(231, 65535) == "231"
+    assert utils.combine_channel_number(231, 1) == "231-1"
 
 
 def test_parse_channel_number() -> None:
