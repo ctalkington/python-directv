065cdf70b7a3ec13f1d1c4bbd96fd5d376bbf760
Chris Talkington <chris@talkingtontech.com>
2020-03-19 20:33:29 -0500

    Create test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,52 @@
+"""Tests for DIRECTV."""
+import asyncio
+
+import pytest
+from aiohttp import ClientSession
+from directv import DIRECTV
+from directv.exceptions import (
+    DIRECTVAccessRestricted,
+    DIRECTVConnectionError,
+    DIRECTVError,
+)
+
+from . Import load_fixture
+
+HOST = "1.2.3.4"
+PORT = 8080
+
+MATCH_HOST = f"{HOST}:{PORT}"
+NON_STANDARD_PORT = 3333
+
+
+@pytest.mark.asyncio
+async def test_update(aresponses):
+    """Test DIRECTV response is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/info/getVersion",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("get-version.json"),
+        ),
+    )
+
+    aresponses.add(
+        MATCH_HOST,
+        "/info/getLocations",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("get-locations.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.update()
+        assert response.info.version == ""
+        assert response.info.receiver_id == ""
+        
