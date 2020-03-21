af3beb93ff4460f77342124ed39ec813a8dd6af4
Chris Talkington <chris@talkingtontech.com>
2020-03-20 23:28:40 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,48 @@
 """Tests for DirecTV Models."""
 import directv.models as models
 
+PROGRAM = {
+  "callsign": "FOODHD",
+  "date": "20070324",
+  "duration": 1791,
+  "episodeTitle": "Spaghetti and Clam Sauce",
+  "expiration": "0",
+  "expiryTime": 0,
+  "isOffAir": false,
+  "isPartial": false,
+  "isPclocked": 1,
+  "isPpv": false,
+  "isRecording": false,
+  "isViewed": true,
+  "isVod": false,
+  "keepUntilFull": true,
+  "major": 231,
+  "minor": 65535,
+  "offset": 263,
+  "programId": "4405732",
+  "rating": "No Rating",
+  "recType": 3,
+  "startTime": 1278342008,
+  "stationId": 3900976,
+  "title": "Tyler's Ultimate",
+  "uniqueId": "6728716739474078694"
+}
+
 
 def test_program() -> None:
     """Test the Program model."""
-    program = models.Program.from_dict({})
+    program = models.Program.from_dict(PROGRAM)
 
     assert program
+    assert program.recorded
+    assert not program.ondemand
+    assert not program.partial
+    assert not program.payperview
+    assert not program.purchased
+    assert not program.recording
+    assert program.channel == "231"
+    assert program.channel_name == "FOODHD"
+    assert program.program_id == ""
+    assert program.program_type == "tvshow"
+    assert program.episode_name == "Spaghetti and Clam Sauce"
+    assert program.duration == 1791
