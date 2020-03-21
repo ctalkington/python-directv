069360f25e2265fa6477343941ba297cf3435e63
Chris Talkington <chris@talkingtontech.com>
2020-03-20 19:43:06 -0500

    API - State (#4)

--- directv/models.py
+++ directv/models.py
@@ -1,7 +1,7 @@
 """Models for DirecTV."""
 
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 from .exceptions import DIRECTVError
 
@@ -40,6 +40,38 @@ class Location:
             name=data.get("locationName", "Receiver"),
             address=data.get("clientAddr", ""),
         )
+
+
+@dataclass(frozen=True)
+class Program:
+    """Object holding all information of playing program."""
+
+    recorded: bool
+    program_id: int
+    episode_title: str
+    unique_id: int
+
+    @staticmethod
+    def from_dict(data: dict):
+        """Return Info object from DirecTV API response."""
+        unique_id = data.get("uniqueId", None)
+
+        return Program(
+            program_id=data.get("programId", None),
+            episode_title=data.get("episodeTitle", None),
+            recorded=(unique_id is not None),
+            unique_id=unique_id,
+        )
+
+
+@dataclass(frozen=True)
+class State:
+    """Object holding all information of a single receiver client state."""
+
+    authorized: bool
+    available: bool
+    standby: bool
+    program: Optional[Program]
 
 
 class Device:
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -2,6 +2,7 @@
 import pytest
 from aiohttp import ClientSession
 from directv import DIRECTV
+from directv.models import Program, State
 
 from . import load_fixture
 
@@ -74,6 +75,199 @@ async def test_remote(aresponses):
 
 
 @pytest.mark.asyncio
+async def test_state(aresponses):
+    """Test active state is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/info/mode",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("info-mode.json"),
+        ),
+    )
+
+    aresponses.add(
+        MATCH_HOST,
+        "/tv/getTuned",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("tv-get-tuned.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.state()
+
+        assert response
+        assert isinstance(response, State)
+        assert response.available
+        assert not response.standby
+
+        assert isinstance(response.program, Program)
+
+
+@pytest.mark.asyncio
+async def test_state_error_mode(aresponses):
+    """Test state with generic mode error is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/info/mode",
+        "GET",
+        aresponses.Response(
+            status=500,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("info-mode-error.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.state()
+
+        assert response
+        assert isinstance(response, State)
+        assert not response.available
+        assert response.standby
+        assert response.authorized
+
+        assert response.program is None
+
+
+@pytest.mark.asyncio
+async def test_state_error_tuned(aresponses):
+    """Test state with generic tuned error is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/info/mode",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("info-mode.json"),
+        ),
+    )
+
+    aresponses.add(
+        MATCH_HOST,
+        "/tv/getTuned",
+        "GET",
+        aresponses.Response(
+            status=500,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("tv-get-tuned-error.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.state()
+
+        assert response
+        assert isinstance(response, State)
+        assert not response.available
+        assert not response.standby
+        assert response.authorized
+
+        assert response.program is None
+
+
+@pytest.mark.asyncio
+async def test_state_restricted_mode(aresponses):
+    """Test standby state is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/info/mode",
+        "GET",
+        aresponses.Response(
+            status=403,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("info-mode-restricted.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.state()
+
+        assert response
+        assert isinstance(response, State)
+        assert not response.available
+        assert response.standby
+        assert not response.authorized
+
+        assert response.program is None
+
+
+@pytest.mark.asyncio
+async def test_state_restricted_tuned(aresponses):
+    """Test standby state is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/info/mode",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("info-mode.json"),
+        ),
+    )
+
+    aresponses.add(
+        MATCH_HOST,
+        "/tv/getTuned",
+        "GET",
+        aresponses.Response(
+            status=403,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("tv-get-tuned-restricted.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.state()
+
+        assert response
+        assert isinstance(response, State)
+        assert response.available
+        assert not response.standby
+        assert not response.authorized
+
+        assert response.program is None
+
+
+@pytest.mark.asyncio
+async def test_state_standby(aresponses):
+    """Test restricted state is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/info/mode",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("info-mode-standby.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.state()
+
+        assert response
+        assert isinstance(response, State)
+        assert response.available
+        assert response.standby
+
+        assert response.program is None
+
+
+@pytest.mark.asyncio
 async def test_tune(aresponses):
     """Test tune is handled correctly."""
     aresponses.add(
@@ -90,3 +284,25 @@ async def test_tune(aresponses):
     async with ClientSession() as session:
         dtv = DIRECTV(HOST, session=session)
         await dtv.tune("231")
+
+
+@pytest.mark.asyncio
+async def test_tuned(aresponses):
+    """Test tuned is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/tv/getTuned",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("tv-get-tuned.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        dtv = DIRECTV(HOST, session=session)
+        response = await dtv.tuned()
+
+        assert response
+        assert isinstance(response, Program)
